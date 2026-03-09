c88aba3ca53825d50938ae881acfe3cc771478fd
CeloMS <celomikio@gmail.com>
2026-03-09 09:40:01 -0300

    update route user and user model

--- app/models/user.py
+++ app/models/user.py
@@ -3,9 +3,18 @@ from app.database import Base
 
 class User(Base):
     __tablename__ = "users"
+    __exclude__ = {'password_hash'}
+
     id = Column(Integer, primary_key=True, autoincrement=True, index=True)
     email = Column(String(255), unique=True, nullable=False)
     password_hash = Column(String(255), nullable=False)
     created_at = Column(DateTime, nullable=False)
     updated_at = Column(DateTime, nullable=False)
     last_login_at = Column(DateTime, nullable=True)
+
+    def to_dict(self):
+        data = {
+            column.name: getattr(self, column.name)
+            for column in self.__table__.columns if column.name not in self.__exclude__
+        }
+        return data
--- app/routes/route_user.py
+++ app/routes/route_user.py
@@ -1,23 +1,84 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
+from app.database import SessionLocal
+from app.models.user import User
+from datetime import datetime, UTC
 
 user_bp = Blueprint('user', __name__, url_prefix='/users')
 
 @user_bp.route('/', methods = ['POST'])
 def create():
-    pass
-
+    data = request.get_json()
+    if not data:
+        return jsonify({"error": "No data provided"}), 400
+    try:
+        with SessionLocal() as db:
+            if db.query(User).filter_by(email=data['email']).first():
+                return jsonify({"error": f"Email already registered"}), 409
+            now = datetime.now(UTC)
+            user = User(
+                email=data["email"],
+                password_hash=data["password_hash"], #Mudar depois de criar o serviço de password para hash
+                created_at=now,
+                updated_at=now
+            )
+            db.add(user)
+            db.commit()
+            return jsonify(user.to_dict()), 201
+    except KeyError as err:
+        return jsonify({"error": f"Missing argument: {err.args[0]}"}), 400
+    except Exception:
+        return jsonify({"error": "Something has gone wrong"}), 500
+    
 @user_bp.route('/', methods = ['GET'])
 def get_all():
-    pass
+    try: 
+        with SessionLocal() as db:
+            users = db.query(User).all()
+            return jsonify([u.to_dict() for u in users]), 200
+    except Exception:
+        return jsonify({"error": "Something has gone wrong"}), 500
 
 @user_bp.route('/<int:user_id>', methods = ['GET'])
 def get_user(user_id):
-    pass
+    try:
+        with SessionLocal() as db:
+            user = db.get(User, user_id)
+            if user is None:
+                return jsonify({"error": "User not found"}), 404
+            return jsonify(user.to_dict()), 200
+    except Exception:
+        return jsonify({"error": "Something has gone wrong"}), 500
 
-@user_bp.route('/<int:user_id>>', methods = ['PUT'])
+@user_bp.route('/<int:user_id>', methods = ['PUT'])
 def update(user_id):
-    pass
+    data = request.get_json()
+    if not data:
+        return jsonify({"error": "No data provided"}), 400
+    try:
+        with SessionLocal() as db:
+            user = db.get(User, user_id)
+            if user is None:
+                return jsonify({"error": "User not found"}), 404
+            for k, v in data.items():  #Mudar depois de criar o serviço de password para hash
+                if k not in ["id", "created_at"]:
+                    setattr(user, k, v)
+            user.updated_at = datetime.now(UTC)
+            db.commit()
+            return jsonify(user.to_dict()), 200
+    except KeyError as err:
+        return jsonify({"error": f"Missing argument: {err.args[0]}"}), 400
+    except Exception:
+        return jsonify({"error": "Something has gone wrong"}), 500
 
-@user_bp.route('/<int:user_id>>', methods = ['DELETE'])
+@user_bp.route('/<int:user_id>', methods = ['DELETE'])
 def delete(user_id):
-    pass
+    try:
+        with SessionLocal() as db:
+            user = db.get(User, user_id)
+            if user is None:
+                return jsonify({"error": "User not found"}), 404
+            db.delete(user)
+            db.commit()
+            return '', 204
+    except Exception:
+        return jsonify({"error": "Something has gone wrong"}), 500
